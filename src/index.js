a94d68c3aa3bd9df26e64c784ee4004cdb420488
Ferhat Eren Dalçık <f.dalcik@azerion.com>
2022-06-12 22:44:52 +0300

    render 4 arrays of pipe and move them

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const config = {
         // Arcade physics plugin.
         default: "arcade",
         arcade: {
-            gravity: {y: 400},
             // gizmos of game objects
             debug: true
         }
@@ -20,35 +19,58 @@ const config = {
     },
 }
 
+// Loading assets, animations, images etc
+function preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("bird", "assets/bird.png");
+    this.load.image("pipe", "assets/pipe.png");
+}
+
 let bird = null;
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
 const initialPos = {
     x:config.width * 0.1,
     y: config.height / 2
 }
-const flappingVelocity = 300;
 
-// Loading assets, animations, images etc
-function preload() {
-    this.load.image("sky", "assets/sky.png");
-    this.load.image("bird", "assets/bird.png")
-}
+const flappingVelocity = 300;
+const pipesToRender = 4;
+const pipeVerticalDistance = [150,250];
 
 // Initialize assets
 function create() {
 
     this.add.image(0, 0, "sky").setOrigin(0);
     bird = this.physics.add.sprite(initialPos.x, initialPos.y, "bird").setOrigin(0);
+    bird.body.gravity.y = 400;
     this.input.on("pointerdown" , bodyFlap);
     this.input.keyboard.on("keydown_SPACE" , bodyFlap);
+
+    for (let i = 0; i <pipesToRender; i ++) {
+        pipeHorizontalDistance += 400;
+        let pipeRange = Phaser.Math.Between(...pipeVerticalDistance);
+        let pipeVerticalPosition = Phaser.Math.Between(0+20,config.height- 20- pipeRange);
+
+        upperPipe = this.physics.add.sprite(pipeHorizontalDistance,pipeVerticalPosition,"pipe").setOrigin(0,1);
+        lowerPipe = this.physics.add.sprite(upperPipe.x,upperPipe.y + pipeRange,"pipe").setOrigin(0,0);
+
+        upperPipe.body.velocity.x = -200;
+        lowerPipe.body.velocity.x = -200;
+    }
+
+
 }
 
 //60 fps
 // 60 times per second
 // 60 * 16ms = 1000ms
 
-function update(time, delta) {
+function update() {
     if(bird.y > config.height || bird.y < - 0) {
-        alert("you lost")
+        // alert("you lost")
         RestartPlayerFunction();
     }
 
