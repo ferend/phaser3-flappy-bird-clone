9e2588bb53a4c7cb6ba4a954aa9cec9bbb835f90
Ferhat Eren Dalçık <f.dalcik@azerion.com>
2022-07-17 15:30:44 +0300

    add menu buttons and menu creation constructor

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -4,9 +4,23 @@ class BaseScene extends Phaser.Scene{
     constructor(key, config) {
         super(key);
         this.config = config;
+        this.screenCenter = [config.width / 2, config.height / 2];
     }
     create(){
         this.add.image(0, 0, "sky").setOrigin(0);
+    }
+
+    // Constructor for menu creation in base class.
+
+    createMenu(menu, menuEvents){
+        let lastMenuPosY = 0;
+
+        menu.forEach(menuItem =>{
+            const menuPos = [this.screenCenter[0], this.screenCenter[1] + lastMenuPosY];
+           menuItem.textGameObject = this.add.text(...menuPos, menuItem.text , {fontSize : '32px', fill:'#00000f'}).setOrigin(0.5, 1);
+            lastMenuPosY += 42;
+            menuEvents(menuItem);
+        })
     }
 }
 
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -4,11 +4,30 @@ class MenuScene extends BaseScene{
     constructor(config) {
         super('MenuScene', config);
         this.config = config;
+        this.menu = [
+            {scene : 'PlayScene',text :'Play'},
+            {scene : 'ScoreScene',text :'Leaderboard'},
+            {scene : null, text :'Exit'}
+        ]
+
     }
 
     create(){
         super.create();
-        this.scene.start('PlayScene')
+        this.createMenu(this.menu, this.menuEvents.bind(this));
+    }
+
+    menuEvents(menuItem){
+        const textGameObject = menuItem.textGameObject;
+        textGameObject.setInteractive();
+
+        textGameObject.on('pointerover', () => {
+            textGameObject.setStyle({fill : '#8BA363'})
+        });
+        textGameObject.on('pointerout', () => {
+            textGameObject.setStyle({fill : '#00000f'})
+        });
+  
     }
 }
 
